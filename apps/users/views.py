1c0370d7aaaa10b778fceb057137da98af9a65dd
cdntip <fz58358@gmail.com>
2021-10-07 16:04:51 +0800

    修改时区

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.cache import cache
 
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth import authenticate
 from apps.users import forms
+
 # Create your views here.
 import time
 
@@ -14,8 +13,6 @@ from libs.utils import md5
 def is_token(func):
     def inner(request, *args, **kwargs):
         token = request.META.get('HTTP_X_TOKEN', False)
-        # print(token)
-        # print(cache.get(token, False))
         if not token:
             return JsonResponse({'code': 50008, 'message': '登录会话失效，请重新的登录'})
 
@@ -23,13 +20,6 @@ def is_token(func):
         if not user or not user.is_active or not user.is_superuser:
             return JsonResponse({'code': 50008, 'message': '登录会话失效，请重新的登录'})
         request.user = user
-        # if not user.is_active:
-        #     return JsonResponse({'code': 50008, 'message': '该账号禁止登录'})
-        # try:
-        #     login(request, user)
-        # except:
-        #     return JsonResponse({'code': 50008, 'message': '登录异常'})
-
         return func(request, *args, **kwargs)
     return inner
 
@@ -49,15 +39,10 @@ def Login(request):
 
             user = authenticate(username=username, password=password)
 
-            # print(login(request, user))
-            # # print(password)
-            # # print(user_info.password)
-            # ret = check_password(password, user_info.password)
-            # print(ret)
             if not user: return JsonResponse({'code': 20002, 'message': '登录密码错误'})
 
             token = md5("%s%s" %(user.username, time.time())).upper()
-            print(cache.set(token, user, 172800))
+            cache.set(token, user, 172800)
             res_data = {'code': 20000,  'message': '登录成功', 'data': {'token': token}}
             return JsonResponse(res_data)
         return JsonResponse({'code': 20001, 'message': '登录失败', 'error_data': input_data.errors})
@@ -70,7 +55,6 @@ def Info(request):
         return JsonResponse({'code': 50008, 'message': '用户信息不存在，请重新登录'})
     data = {
         'avatar': 'https://t1.picb.cc/uploads/2021/10/05/wXMX1y.th.jpg',
-        # 'introduction': '.',
         'name': userinfo.username,
         'username': userinfo.username,
         'create_time': userinfo.date_joined.strftime("%Y-%m-%d %H:%M:%S"),
