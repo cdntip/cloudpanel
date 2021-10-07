7ab73665dc2f1014f5a93170d80b452418abd12d
cdntip <fz58358@gmail.com>
2021-10-07 09:08:11 +0800

    add scripts

---
+++ apps/users/management/commands/init_db.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.core.management import execute_from_command_line
+from django.conf import settings
+
+class Command(BaseCommand):
+    help = '初始化/更新数据库'
+
+    def handle(self, *args, **options):
+        args = ['manage.py', 'makemigrations']
+        apps = [x.split('.')[-1] for x in settings.INSTALLED_APPS if x.startswith('apps.')]
+        execute_from_command_line(args + apps)
+        execute_from_command_line(['manage.py', 'migrate'])
+        self.stdout.write(self.style.SUCCESS('  初始化/更新成功'))
